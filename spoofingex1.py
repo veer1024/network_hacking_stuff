60214a2e461c730b17f60facf5f9a988029292df
veer1024 <viraj1024.1024@gmail.com>
2020-12-10 03:38:06 +0530

    some change

--- spoofingex1.py
+++ spoofingex1.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 from scapy.all import *
 print("Sending SPOOFED ICMP packet...")
-ip=IP(src="192.168.43.246",dst="192.168.43.246")
+#targetmac="ff:ff:ff:ff:ff:ff"
+fakemacforvictim="08:00:27:f9:64:17"
+ether=Ether()
+#ether.dst=targetmac
+ether.src=fakemacforvictim
+ip=IP(src="192.168.43.136",dst="8.8.8.8")
 icmp=ICMP()
 data='Y\x1c\x8a_\x00\x00\x00\x00\xd5o\x03\x00\x00\x00\x00\x00\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !"#$%&\'()*+,-./01234567'
-pkt=ip/icmp/data
+pkt=ether/ip/icmp/data
 pkt.show()
-send(pkt,verbose=0)
+sendp(pkt)
+#send(pkt,verbose=0)
